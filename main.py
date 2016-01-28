690870842f6eb2ed984c309410eb341c7cb0e099
unknown <qherrero@me-jmcginle2.ic.ac.uk>
2016-01-28 17:00:46 +0000

    2 motors and speed implementation

--- main.py
+++ main.py
@@ -5,45 +5,64 @@ from struct import pack, unpack
 from socket import socket, AF_INET, SOCK_STREAM
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import Screen
 from kivy import config
 config.Config.set('input', 'mouse', 'mouse,disable_multitouch')
 
 
-class MainScreen(BoxLayout):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.motor = None
-        self.motor_connected = False
-        self.thread_on = True
-        Thread(target=self.get_position).start()
+class MotorScreen(Screen):
+    def __init__(self, ip=None, name=None, **kwargs):
+        super(MotorScreen, self).__init__(**kwargs)
+        self.name = name
+        # self.motor contains the class to talk to the motor through tcp/ip
+        self.motor = Motor_TCPIP(ip, 2317)
+        #   Try to connect
+        try:
+            self.motor.open_socket()
+            self.ids.device.text = name + ' Status : Device connected.'
+            self.ids.btn_on.disabled = False
+            # thread_on is just here to close properly the thread when window
+            # is closed
+            self.thread_on = True
+            Thread(target=self.get_position).start()
+        except:
+            self.ids.device.text = name + ' Status : Device not connected.'
+            self.on_or_off(False)
+            self.ids.btn_on.disabled = True
+
+    def shift_screen(self):
+        if self.name == 'Motor1':
+            self.manager.current = 'Motor2'
+        else:
+            self.manager.current = 'Motor1'
 
     def get_position(self):
+        # This loop reads position if the motor is on
         while self.thread_on:
-            if not self.motor_connected:
-                try:
-                    self.motor = Motor('192.168.39.5', 2317)
-                    self.motor_connected = True
-                    self.ids.device.text = 'Status : Device connected.'
-                    self.ids.btn_on.disabled = False
-                except:
-                    self.ids.device.text = 'Status : Device not connected.'
-                    self.on_or_off(False)
-                    self.ids.btn_on.disabled = True
-                    self.motor_connected = False
-                sleep(2)
-            else:
-                try:
-                    pos = self.motor.motor_read_pos()
-                    self.ids.cur_pos.text = str(pos)
-                except:
-                    self.motor_connected = False
-                sleep(0.5)
+            try:
+                pos = self.motor.motor_read_pos()
+                self.ids.cur_pos.text = "[b]Current Position "
+                self.ids.cur_pos.text += "[color=#008000]" + str(pos)
+                self.ids.cur_pos.text += "[/color] ?[/b]"
+                speed = self.motor.motor_read_speed()
+                self.ids.cur_speed.text = "[b]Current Speed "
+                self.ids.cur_speed.text += "[color=#008000]%.2f" % speed
+                self.ids.cur_speed.text += "[/color] ?[/b]"
+            except:
+                self.motor.close_socket()
+                self.ids.device.text = 'Status : Device not connected.'
+                self.on_or_off(False)
+                self.ids.btn_on.disabled = True
+                break
+            sleep(0.5)
         return 0
 
     def on_or_off(self, on_off):
+        # Just to disable or enable buttons
         self.ids.btn_on.disabled = on_off
         self.ids.btn_off.disabled = not on_off
         self.ids.btn_pos.disabled = not on_off
+        self.ids.btn_speed.disabled = not on_off
 
     def on(self):
         self.motor.motorcontrol_on()
@@ -59,7 +78,18 @@ class MainScreen(BoxLayout):
             self.motor.motor_move_pos(pos)
 
     def set_speed(self):
-        print('set speed')
+        speed = int(self.ids.input_speed.text)
+        if 1 <= speed <= 300:
+            self.motor.set_speed(speed)
+
+
+class MainScreen(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        screen = MotorScreen(ip='192.168.39.5', name='Motor1')
+        self.ids.rootscreen.add_widget(screen)
+        screen = MotorScreen(ip='192.168.39.6', name='Motor2')
+        self.ids.rootscreen.add_widget(screen)
 
 
 class MainApp(App):
@@ -71,19 +101,37 @@ class MainApp(App):
         return self.root
 
     def on_stop(self):
-        self.root.thread_on = False
+        screen1 = self.root.ids.rootscreen.get_screen('Motor1')
+        screen2 = self.root.ids.rootscreen.get_screen('Motor2')
         try:
-            self.root.motor.s.close()
+            screen1.thread_on = False
+            screen1.motor.motorcontrol_off()
+            screen1.motor.close_socket()
+        except:
+            pass
+        try:
+            screen2.thread_on = False
+            screen2.motor.motorcontrol_off()
+            screen2.motor.close_socket()
         except:
             pass
 
 
-class Motor(object):
+class Motor_TCPIP(object):
     def __init__(self, tcp_ip, tcp_port):
         # Open the connexion
         self.buffer = 2048
+        self.tcp_ip = tcp_ip
+        self.tcp_port = tcp_port
         self.s = socket(AF_INET, SOCK_STREAM)
-        self.s.connect((tcp_ip, tcp_port))
+        self.s.settimeout(1)
+
+    def open_socket(self):
+        self.s.connect((self.tcp_ip, self.tcp_port))
+
+    def close_socket(self):
+        self.s.shutdown(2)
+        self.s.close()
 
     def motorcontrol_on(self):
         # Command to turn the motor on, the ID corresponds to the DriveManager
@@ -91,11 +139,9 @@ class Motor(object):
         # function. The value has been guessed by observing the behavior of
         # motor and sniffing packets.
         self.packet_sender(2, 229, 128, 'int32')
-        print("Done")
 
     def motorcontrol_off(self):
         self.packet_sender(2, 229, 0, 'int32')
-        print("Done")
 
     def motor_move_pos(self, position):
         # Only one packet is containing the position information. The others
@@ -105,15 +151,24 @@ class Motor(object):
         self.packet_sender(2, 301, 0, 'uint16')
         self.packet_sender(2, 230, position, 'int32')
         self.packet_sender(2, 229, 1153, 'int32')
-        print("Done")
+
+    def set_speed(self, speed):
+        # Acceleration and decceleration are 232 and 233 float if needed
+        self.packet_sender(2, 231, speed, 'float')
 
     def motor_read_pos(self):
-        # Packet to read positions. Note it's the only function with a
+        # Packet to read positions. Note it's a function with a
         # "return". It's because we need to use the returning packet.
         data = self.packet_sender(1, 276, 1, 'int32')
         data = data[len(data) - 4:len(data)]
 
         return unpack('<i', data)[0]
+
+    def motor_read_speed(self):
+        data = self.packet_sender(1, 281, 1, 'int32')
+        data = data[len(data) - 4:len(data)]
+
+        return unpack('<f', data)[0]
 
     def packet_sender(self, p_type, id_value, value, type_v):
         # This is building packets, the explanation can be found in Robert's
